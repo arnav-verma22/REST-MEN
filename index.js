7df1b920e7eb4390d4e1a994c570354c0bc30adc
arnav-verma22 <arn.verma22@gmail.com>
2021-07-23 17:50:06 +0530

    main file

---
+++ index.js
@@ -0,0 +1,57 @@
+
+const express = require('express')
+const mongoose = require('mongoose')
+const url = 'mongodb://localhost/AlienDBex'
+
+const app = express()
+
+mongoose.connect(url, { useNewUrlParser: true })
+const con = mongoose.connection
+
+con.on('open', () => console.log('Connected...'))
+
+const alrouter = require('./routers/alien')
+
+app.use(express.json)
+
+app.use('/aliens', alrouter)
+
+app.get('/', (req, res) => {
+    return res.status(200).json({
+        "status": true,
+        "message": "Rest API"
+    });
+});
+
+app.listen(9000, () => {
+    console.log('Server started')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+let add = function(x, y)
+{
+    return x + y
+}
+console.log(add(7,4))
+sa
+console.log(add(45,43), end='')
+process.stdout.write("Arnav \nverma: ");
+
+students.array.forEach(element => {
+  
+});
+*/
+
